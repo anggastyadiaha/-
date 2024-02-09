58213b675c5093999ed0fa7413339f30170835b9
anggastyadiaha <23521062@std.stei.itb.ac.id>
2024-02-09 21:38:52 +0700

    add database bsi_app.sql

--- express/index.js
+++ express/index.js
@@ -16,36 +16,72 @@ const pool = new Pool({
 
 app.use(bodyParser.json());
 
-app.get('/wallet/:id', async (req, res, id) => {
-    const { page = 1, limit = 10 } = req.query;
-    const offset = (page - 1) * limit;
-    
-    const query = 'SELECT * FROM wallet WHERE id= {id}';
-
-    
+// Fungsi Read (SELECT)
+app.get('/wallet/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const query = 'SELECT * FROM wallet WHERE id = $1';
+  
   try {
-    const result = await pool.query(query, [offset, limit]);
-
-    // Menghitung total entri tanpa paginasi
-    const totalQuery = 'SELECT COUNT(*) FROM wallet';
-    const totalResult = await pool.query(totalQuery);
-    const totalEntries = parseInt(totalResult.rows[0].count, 10);
-
-    const totalPages = Math.ceil(totalEntries / limit);
-
-    res.json({
-      data: result.rows,
-      pagination: {
-        page: parseInt(page, 10),
-        limit: parseInt(limit, 10),
-        totalEntries,
-        totalPages,
-      },
-    });
+    const result = await pool.query(query, [id]);
+    res.json(result.rows);
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
-
 });
 
+// Fungsi Read Transactions (SELECT)
+app.get('/transaction/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const query = 'SELECT * FROM transaction WHERE wallet_id = $1';
+  
+  try {
+    const result = await pool.query(query, [id]);
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Fungsi Patch (Partial Update)
+app.patch('/wallet/:id', async (req, res) => {
+  const Id = req.params.id;
+  const balance = req.body.balance;
+  const updateFields = req.body;
+
+  if (Object.keys(updateFields).length === 0) {
+    return res.status(400).send('Bad Request: No fields to update');
+  }
+  const query = 'UPDATE wallet SET balance = balance + $1 WHERE id=$2 RETURNING *';
+  const values = [balance, Id];
+
+  try {
+    const result = await pool.query(query, values);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Fungsi Create (INSERT)
+app.post('/wallet/:id', async (req, res) => {
+  const Id = req.params.id;
+  // const amount = req.body.amount;
+  const query = 'INSERT into transaction (wallet_id, name, description, amount, type) VALUES ($1, $2, $3, $4, $5) RETURNING *;';
+  const {name = "Top-Up", description = "Top-Up Saldo", amount, type = "IN"} = req.body
+  const values = [Id, name, description, amount, type];
+
+  try {
+    const result = await pool.query(query, values);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
